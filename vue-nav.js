4eed000195905300a4b9eb02d8def371a7dceed4
周亮俊 <liangjun.zhou@ele.me>
2016-08-01 19:45:32 +0800

    可灵活调整显示的页码数

--- vue-nav.js
+++ vue-nav.js
@@ -1,63 +1,126 @@
 (function(){
-var tm = '<div class="page-bar">'+
-            '<ul>'+
-            '<li v-if="cur!=1"><a v-on:click="cur--">上一页</a></li>'+
-            '<li v-for="index in indexs"  v-bind:class="{ active: cur == index}">'+
-                '<a v-on:click="btnClick(index)">{{ index }}</a>'+
-                '</li>'+
-                '<li v-if="cur!=all"><a v-on:click="cur++">下一页</a></li>'+
-                '<li><a>共<i>{{all}}</i>页</a></li>'+
-            '</ul>'+
+    var tm = '<div class="page-bar">'+
+        '<ul>'+
+        '<li v-if="cur!=1"><a v-on:click="cur--">上一页</a></li>'+
+        '<li v-for="index in indexs"  v-bind:class="{ active: cur == index}">'+
+        '<a v-on:click="btnClick(index)">{{ index }}</a>'+
+        '</li>'+
+        '<li v-if="cur!=all"><a v-on:click="cur++">下一页</a></li>'+
+        '<li><a>共<i>{{all}}</i>页</a></li>'+
+        '</ul>'+
         '</div>'
 
 
 
 
+    // var navBar = Vue.extend({
+    //     template: tm,
+    //     props: ['cur', 'all'],
+    //     data: function(){
+    //         return {
+    //
+    //         }
+    //     },
+    //     computed: {
+    //         indexs: function(){
+    //           var left = 1
+    //           var right = this.all
+    //           var ar = []
+    //           if(this.all>= 11){
+    //             if(this.cur > 5 && this.cur < this.all-4){
+    //                     left = this.cur - 5
+    //                     right = this.cur + 4
+    //             }else{
+    //                 if(this.cur<=5){
+    //                     left = 1
+    //                     right = 10
+    //                 }else{
+    //                     right = this.all
+    //                     left = this.all -9
+    //                 }
+    //             }
+    //          }
+    //         while (left <= right){
+    //             ar.push(left)
+    //             left ++
+    //         }
+    //         return ar
+    //        }
+    //     },
+    //     methods: {
+    //         btnClick: function(data){
+    //             if(data != this.cur){
+    //                 this.cur = data
+    //                 this.$dispatch('btn-click',data)
+    //
+    //             }
+    //         }
+    //     }
+    //
+    //
+    //
+    // })
+
+
+
+
+    // var limit=10;
+    // var half=Math.floor(limit/2);
     var navBar = Vue.extend({
         template: tm,
-        props: ['cur', 'all'],
+        props: ['cur', 'all','page_length'],
         data: function(){
-            
+            return {
+
+            }
         },
         computed: {
             indexs: function(){
-              var left = 1
-              var right = this.all
-              var ar = [] 
-              if(this.all>= 11){
-                if(this.cur > 5 && this.cur < this.all-4){
-                        left = this.cur - 5
-                        right = this.cur + 4
-                }else{
-                    if(this.cur<=5){
-                        left = 1
-                        right = 10
+                var limit=this.page_length;
+                var half=Math.floor(limit/2);
+
+                var left = 1
+                var right = this.all
+                var ar = []
+                if(this.all>= limit+1){
+                    if(this.cur > half && this.cur < this.all-half){
+                        left = this.cur - half
+                        right = this.cur + half
                     }else{
-                        right = this.all
-                        left = this.all -9
+                        if(this.cur<=half){
+                            left = 1
+                            right = limit
+                        }else{
+                            right = this.all
+                            left = this.all -(limit-1)
+                        }
                     }
                 }
-             }
-            while (left <= right){
-                ar.push(left)
-                left ++
-            }   
-            return ar
-           }
+                while (left <= right){
+                    ar.push(left)
+                    left ++
+                }
+                return ar
+            }
         },
         methods: {
             btnClick: function(data){
                 if(data != this.cur){
-                    this.cur = data 
-                    this.$dispatch('btn-click',data) 
+                    this.cur = data
+                    this.$dispatch('btn-click',data)
 
                 }
             }
         }
 
-
-        
     })
+
+
+
+
+
+
+
 
     window.Vnav = navBar
 
